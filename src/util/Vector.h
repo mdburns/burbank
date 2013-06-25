8f5ea8f6eab2fc95f0a019435a3f302a4b603031
Mike Burns <m@cs.brown.edu>
2013-06-25 03:28:11 -0400

    Finished/touched up Vector class.

--- src/util/Vector.h
+++ src/util/Vector.h
@@ -1,189 +1,226 @@
 #ifndef __VECTOR_H__
 #define __VECTOR_H__
 
+#include <cstdlib>
 #include <vector>
 #include <math.h>
 
 #define REAL float 
 
-template <class T, int n>
+/* T MUST BE A NON-INTEGER TYPE */
+template <typename T, int n>
 class Vector {
   public:
+    // constructor
     Vector( const std::vector<T> & vec );
-    ~Vector();
 
-    const T& operator[](int i);                                     //Access element
-    const Vector<T,n>& operator*(Vector<T,n> v);     //component-wise mult
-    const Vector<T,n>& operator+(Vector<T,n> v);     //component-wise add
-    const Vector<T,n>& operator/(Vector<T,n> v);     //compoment-wise divide
-    const Vector<T,n>& operator-(Vector<T,n> v);     //component-wise subtract 
-    const Vector<T,n>& operator-();       //unary negation
-    const T& dot(Vector<T,n> v1, Vector<T,n> v2);       //dot product
-    const Vector<REAL,n>& normalize();                  //normalize
-    const T& mag();                                     //magnitude
-    const Vector<T,n>& scale(Vector<T,n>, T alpha);     //Scalar multiplication
-    void scale(T alpha);                  //mutating scalar multiplication
-    void mult(Vector<T,n> v);             //mutating multiplication
-    void add(Vector<T,n> v);              //mutating addition
-    void div(Vector<T,n> v);              //mutating division
-    void sub(Vector<T,n> v);              //mutating subtraction
-    const REAL & angleBetween(Vector<T,n> v1, Vector<T,n> v2);  //angle between two vectors
-    const REAL & angleWith(Vector<T,n> v2);                     //angle between this and another vector
+    // copy constructor
+    Vector( const Vector<T,n> & vec );
+
+    // element access
+    T operator[](int i) const;
+
+    // component-wise operations
+    Vector<T,n> operator*( const Vector<T,n> & v ) const;
+    Vector<T,n> operator+( const Vector<T,n> & v ) const;
+    Vector<T,n> operator/( const Vector<T,n> & v ) const;
+    Vector<T,n> operator-( const Vector<T,n> & v ) const;
+    Vector<T,n> operator-() const;
+
+    // in-place component-wise operations
+    void mult( const Vector<T,n> & v );
+    void add(  const Vector<T,n> & v );
+    void div(  const Vector<T,n> & v );
+    void sub(  const Vector<T,n> & v );
+
+    // vector operations
+    T mag() const;
+    T dot( const Vector<T,n> & v ) const;
+    Vector<T,n> & normalize() const;
+    Vector<T,n> & scale( const T & alpha ) const;
+
+    // in-place vector operations
+    void inormalize();
+    void iscale( const T & alpha );
+
+    // convenience methods
+    T angleTo( const Vector<T,n> & v ) const;
 
   private:
     std::vector<T> _vec;
 };
 
 /* Constructor */
-template <class T, int n>
-Vector<T, n>::Vector( const std::vector<T> & vec ) {
-  if(vec.size() != n) {
+template <typename T, int n>
+Vector<T,n>::Vector( const std::vector<T> & vec ) {
+  if( vec.size() != n ) {
     std::cerr << "Improper size: got " << vec.size() << ", expected " << _vec.size() << std::endl;
-    std::exit(EXIT_FAILURE);
+    std::exit( EXIT_FAILURE );
   }
 
   _vec = vec;
 } 
 
+/* Copy constructor */
+template <typename T, int n>
+Vector<T,n>::Vector( const Vector<T,n> & copy ) {
+  _vec = std::vector<T>( copy._vec );
+}
+
 /* retrieval operator [] */
-template <class T, int n>
-T & Vector<T,n>::operator[](int i) {
-  if(i < 0 || i >= n) {
-    std::cerr << "Index out of bounds [" << i "]" << std::endl;
-    std::exit(EXIT_FAILURE);
-  } 
+template <typename T, int n>
+T Vector<T,n>::operator[]( int i ) const {
+  if( i < 0 || i >= n ) {
+    std::cerr << "Index out of bounds [" << i << "]" << std::endl;
+    std::exit( EXIT_FAILURE );
+  }
+
   return _vec[i];
 }
 
 /* vector-vector multiplication (component-wise) */
-template <class T, int n>
-Vector<T,n> & Vector<T,n>::operator*(Vector<T,n> v) {
-  std::vector dummy = new std::vector;
-  for(int i = 0; i < n; ++i) {
-    dummy[i] = _vec[i] * v[i];
-  }
-  return new Vector<T,n>( dummy );
-}
+template <typename T, int n>
+Vector<T,n> Vector<T,n>::operator*( const Vector<T,n> & v ) const {
+  std::vector<T> copy = std::vector<T>( _vec );
 
-/* vector-vector addition (component-wise) */
-template <class T, int n>
-Vector<T,n> & Vector<T,n>::operator+(Vector<T,n> v) {
-  std::vector dummy = new std::vector;
-  for(int i = 0; i < n; ++i) {
-    dummy[i] = _vec[i] + v[i];
-  }
-  return new Vector<T,n>( dummy );
+  for( int i = 0; i < n; ++i )
+    copy[i] *= v[i];
+
+  return Vector<T,n>( copy );
 }
 
 /* vector-vector division (component-wise) */
-template <class T, int n>
-Vector<T,n> & Vector<T,n>::operator/(Vector<T,n> v) {
-  std::vector dummy = new std::vector;
-  for(int i = 0; i < n; ++i) {
-    dummy[i] = _vec[i] / v[i];
-  }
-  return new Vector<T,n>( dummy );
+template <typename T, int n>
+Vector<T,n> Vector<T,n>::operator/( const Vector<T,n> & v ) const {
+  std::vector<T> copy = std::vector<T>( _vec );
+
+  for( int i = 0; i < n; ++i )
+    copy[i] /= v[i];
+
+  return Vector<T,n>( copy );
+}
+
+/* vector-vector addition (component-wise) */
+template <typename T, int n>
+Vector<T,n> Vector<T,n>::operator+( const Vector<T,n> & v ) const {
+  std::vector<T> copy = std::vector<T>( _vec );
+
+  for( int i = 0; i < n; ++i )
+    copy[i] += v[i];
+
+  return Vector<T,n>( copy );
 }
 
 /* vector-vector subtraction (component-wise) */
-template <class T, int n>
-Vector<T,n> & Vector<T,n>::operator-(Vector<T,n> v) {
-  std::vector dummy = new std::vector;
-  for(int i = 0; i < n; ++i) {
-    dummy[i] = _vec[i] - v[i];
-  }
-  return new Vector<T,n>( dummy );
+template <typename T, int n>
+Vector<T,n> Vector<T,n>::operator-( const Vector<T,n> & v ) const {
+  std::vector<T> copy = std::vector<T>( _vec );
+
+  for( int i = 0; i < n; ++i )
+    copy[i] -= v[i];
+
+  return Vector<T,n>( copy );
 }
 
-/* unary negation */
-template <class T, int n>
-Vector<T,n> & Vector<T,n>::operator-() {
-  std::vector dummy = new std::vector;
-  for(int i = 0; i < n; ++i) {
-    dummy[i] = -vec[i];
-  }
-  return new Vector<T,n>( dummy );
+/* unary negation (component-wise) */
+template <typename T, int n>
+Vector<T,n> Vector<T,n>::operator-() const {
+  std::vector<T> copy = std::vector<T>( _vec );
+
+  for( int i = 0; i < n; ++i )
+    copy[i] = -copy[i];
+
+  return Vector<T,n>( copy );
 }
 
 /* dot product */
-template <class T, int n>
-T & Vector<T,n>::dot(Vector<T,n> v1, Vector<T,n> v2) {
-  T acc = 0;
-  for(int i = 0; i < n; ++i) {
-    acc += (v1[i] * v2[i]);
-  }
+template <typename T, int n>
+T Vector<T,n>::dot( const Vector<T,n> & v ) const {
+  T acc = 0.f;
+
+  for( int i = 0; i < n; ++i )
+    acc += ( _vec[i] * v[i] );
+
   return acc;
 }
 
 /* magnitude */
-template <class T, int n>
-T & Vector<T,n>::mag(Vector<T,n> v) {
-  return sqrt(Vector<T,n>::dot(v, v));
+template <typename T, int n>
+T Vector<T,n>::mag() const {
+  return sqrt( _vec.dot( _vec ) );
 }
 
 /* normalize */
-template <class T, int n>
-Vector<REAL,n> & Vector<T,n>::normalize(Vector<T,n> v) {
-  std::vector dummy = new std::vector;
-  int magnitude = v.mag();
-  for(int i = 0; i < n; ++i) {
-    dummy[i] = v[i] / magnitude;
-  }
-  return new Vector<REAL,n>( dummy );
+template <typename T, int n>
+Vector<T,n> & Vector<T,n>::normalize() const {
+  std::vector<T> copy = std::vector<T>( _vec );
+  T magnitude = mag();
+
+  for(int i = 0; i < n; ++i)
+    copy[i] = copy[i] / magnitude;
+
+  return Vector<T,n>( copy );
+}
+
+/* in-place normalize */
+template <typename T, int n>
+void Vector<T,n>::inormalize() {
+  T magnitude = mag();
+
+  for(int i = 0; i < n; ++i)
+    _vec[i] = _vec[i] / magnitude;
 }
 
 /* scalar multiplication */
-Vector<T,n> & Vector<T,n>::scale(Vector<t,n> v, T alpha) {
-  std::vector dummy = new std::vector;
-  for(int i = 0; i < n; ++i) {
-    dummy[i] = v[i] * alpha;
-  }
-  return new Vector<T,n>( dummy );
+template <typename T, int n>
+Vector<T,n> & Vector<T,n>::scale( const T & alpha ) const {
+  std::vector<T> copy = std::vector<T>( _vec );
+
+  for ( int i = 0; i < n; ++i )
+    copy[i] = copy[i] * alpha;
+
+  return Vector<T,n>( copy );
 }
 
 /* mutating scalar multiplication */
-void Vector<T,n>::scale(T alpha) {
-  for(int i = 0; i < n; ++i) {
+template <typename T, int n>
+void Vector<T,n>::iscale( const T & alpha ) {
+  for ( int i = 0; i < n; ++i )
     _vec[i] *= alpha;
-  }
 }
 
 /* mutating multiplication */
-void Vector<T,n>::mult(Vector<T,n> v) {
-  for(int i = 0; i < n; ++i) {
+template <typename T, int n>
+void Vector<T,n>::mult( const Vector<T,n> & v ) {
+  for ( int i = 0; i < n; ++i )
     _vec[i] *= v[i];
-  }
 }
 
 /* mutating addition */
-void Vector<T,n>::add(Vector<T,n> v) {
-  for(int i = 0; i < n; ++i) {
+template <typename T, int n>
+void Vector<T,n>::add( const Vector<T,n> & v ) {
+  for ( int i = 0; i < n; ++i )
     _vec[i] += v[i];
-  }
 }
 
 /* mutating division */
-void Vector<T,n>::div(Vector<T,n> v) {
-  for(int i = 0; i < n; ++i) {
+template <typename T, int n>
+void Vector<T,n>::div( const Vector<T,n> & v ) {
+  for ( int i = 0; i < n; ++i )
     _vec[i] /= v[i];
-  }
 } 
 
 /* mutating subtration */
-void Vector<T,n>::sub(Vector<T,n> v) {
-  for(int i = 0; i < n; ++i) {
+template <typename T, int n>
+void Vector<T,n>::sub( const Vector<T,n> & v ) {
+  for ( int i = 0; i < n; ++i )
     _vec[i] -= v[i];
-  }
-}
-
-/* angle between two vectors */
-REAL & Vector<T,n>::angleBetween(Vector<T,n> v1, Vector<T,n> v2) {
-  return acos(Vector<T,n>::dot(v1, v2) / (v1.mag() * v2.mag()));
 }
 
 /* angle between this and another vector */
-REAL & Vector<T,n>::angleTo(Vector<T,n> v) {
-  return acos(Vector<T,n>::dot(this, v) / (this.mag() * v.mag()));
+template <typename T, int n>
+T Vector<T,n>::angleTo( const Vector<T,n> & v ) const {
+  return acos( dot(v) / ( mag() * v.mag() ) );
 }
 
 #endif
